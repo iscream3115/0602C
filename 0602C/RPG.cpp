c0a7f0071e4f9814b3d1c569a9af8de8d5ce2966
관희 이 <rumble3115@naver.com>
2023-06-08 22:14:31 +0900

    RPG 초안  ver0.1

---
+++ 0602C/RPG.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+struct player
+{
+	int hp;
+	int mp;
+	int stamina;
+	int atk;
+	int def;
+	int ag;
+
+};
+
+
+int main()
+{
+
+	player warrior;
+	player knight;
+	player thief;
+	player hunter;
+	player bum;
+
+	printf("######      ##    ######   ###  ##            #####    #####    ##  ##   ####     #####   \n");
+	printf(" ##  ##    ####    ##  ##   ## ##            ##   ##  ##   ##   ##  ##    ##     ##   ##  \n");
+	printf(" ##  ##   ##  ##   ##  ##   ####             ##       ##   ##   ##  ##    ##     ##        \n");
+	printf(" ##  ##   ######   #####    ###               #####   ##   ##   ##  ##    ##      #####   \n");
+	printf(" ##  ##   ##  ##   ####     ####                  ##  ##   ##   ##  ##    ##          ##  \n");
+	printf(" ##  ##   ##  ##   ## ##    ## ##            ##   ##  ##   ##   ##  ##    ## ##  ##   ##   \n");
+	printf("######    ##  ##  ###  ##  ###  ##            #####    #####     ####    ######   #####   \n");
+
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("			     PRESS ANY BUTTON											\n");
+
+
+	return 0;
+}
